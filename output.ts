6b4bf298f7ae2e7ff3fb54d27b9c046d2ed3b70b
Travis <travis.kuhl@gmail.com>
2022-03-01 08:36:39 -0800

    action cleanup

--- output.ts
+++ output.ts
@@ -1,10 +1,24 @@
 import { getInput, setOutput } from "./core.ts";
+import { fsRead } from "./fs/read.ts";
 
 async function main() {
   const name = getInput("name");
   const value = getInput("value");
 
-  await setOutput(name, value);
+  if (!value.startsWith("file://")) {
+    await setOutput(name, value);
+    return;
+  }
+
+  const decode = getInput("decode", false);
+
+  await setOutput(
+    name,
+    await fsRead({
+      src: value.replace("file://", ""),
+      decode,
+    }),
+  );
 }
 
 if (import.meta.main) {
